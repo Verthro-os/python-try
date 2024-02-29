f5cab2cecc9d2fd1430a7ad7c4d27bea62d2ea87
Luca1202 <74714663+Luca1202@users.noreply.github.com>
2024-02-29 21:57:38 +0100

    Added Advertisement List and Delete

--- Components/app.py
+++ Components/app.py
@@ -15,9 +15,9 @@ app = Flask(__name__)
 UPLOAD_FOLDER = 'static/images'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-#chawin only dont change!!
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/chs/Desktop/flaskproject21/python-try/Components/instance/carvis.db'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users\Verty\Documents\python try\Components\instance\carvis.db' #Mickey Test
+# chawin only dont change!!
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/chs/Desktop/flaskproject21/python-try/Components/instance/carvis.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users\Verty\Documents\python try\Components\instance\carvis.db' #Mickey Test
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///carvis.db'
 db = SQLAlchemy(app)
 app.secret_key = 'eb3d197e1633fd5193f89ff8b2887923d12645b647a97893'
@@ -28,6 +28,7 @@ class UserLevels(PyEnum):
     SALESPERSON = 'Salesperson'
     SUPERADMIN = 'Superadmin'
 
+
 class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
@@ -35,12 +36,14 @@ class User(db.Model):
     user_level = db.Column(db.Enum(UserLevels), nullable=False)
     personal_info = db.relationship('PersonalInformation', backref='user', uselist=False)
 
+
 class PersonalInformation(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.user_id', ondelete='CASCADE'), primary_key=True)
     full_name = db.Column(db.String(100))
     email = db.Column(db.String(100))
     phone_number = db.Column(db.String(20))
     address = db.Column(db.Text)
+
 
 class CarModel(db.Model):
     model_id = db.Column(db.Integer, primary_key=True)
@@ -50,12 +53,13 @@ class CarModel(db.Model):
     mileage = db.Column(db.Integer)
     price = db.Column(db.Numeric(10, 2))
     description = db.Column(db.Text)
-    image_url = db.Column(db.String(255))  
-    image_url_2 = db.Column(db.String(255))  
-    image_url_3 = db.Column(db.String(255))  
-    fuel_type = db.Column(db.Text)  
-    safety_features = db.Column(db.Text)  
-    additional_details = db.Column(db.Text) 
+    image_url = db.Column(db.String(255))
+    image_url_2 = db.Column(db.String(255))
+    image_url_3 = db.Column(db.String(255))
+    fuel_type = db.Column(db.Text)
+    safety_features = db.Column(db.Text)
+    additional_details = db.Column(db.Text)
+
 
 class Order(db.Model):
     order_id = db.Column(db.Integer, primary_key=True)
@@ -65,15 +69,16 @@ class Order(db.Model):
     order_date = db.Column(db.Date)
     total_price = db.Column(db.Numeric(10, 2))
     negotiated_price = db.Column(db.Numeric(10, 2))
-    name = db.Column(db.String(100))  
-    email = db.Column(db.String(100))  
-    country = db.Column(db.String(50))  
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    country = db.Column(db.String(50))
     order_status = db.Column(db.Integer, default=1)  # Assuming default is 'pending'
 
     # Relationships
     user = db.relationship('User', backref=db.backref('orders', lazy=True))
     car_model = db.relationship('CarModel', backref=db.backref('orders', lazy=True))
     advertisement = db.relationship('Advertisement', backref=db.backref('orders', lazy=True))
+
 
 class SalesHistory(db.Model):
     sale_id = db.Column(db.Integer, primary_key=True)
@@ -83,6 +88,7 @@ class SalesHistory(db.Model):
     sale_date = db.Column(db.Date)
     total_price = db.Column(db.Numeric(10, 2))
 
+
 class Advertisement(db.Model):
     ad_id = db.Column(db.Integer, primary_key=True)
     model_id = db.Column(db.Integer, db.ForeignKey('car_model.model_id', ondelete='CASCADE'))
@@ -90,12 +96,14 @@ class Advertisement(db.Model):
     ad_description = db.Column(db.Text)
     ad_expiry_date = db.Column(db.Date)
     is_new = db.Column(db.Boolean)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id', ondelete='CASCADE'))
+    is_hidden = db.Column(db.Boolean)
+
 
 class Salesperson(db.Model):
     salesperson_id = db.Column(db.Integer, db.ForeignKey('user.user_id', ondelete='CASCADE'), primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.user_id', ondelete='CASCADE'))
     commission_rate = db.Column(db.Numeric(5, 2))
-
 
 
 @app.route('/create_account')
@@ -142,7 +150,6 @@ def create_account():
     return redirect(url_for('login'))
 
 
-
 @app.route('/login')
 def show_login_account_form():
     return render_template('user_login.html')
@@ -163,9 +170,8 @@ def login():
         elif existing_user.user_level == UserLevels.SUPERADMIN:
             return redirect(url_for('homepage'))
         else:
-            return redirect(url_for('homepage'))
-    
-    
+            return redirect(url_for('account'))
+
     return render_template('user_login.html', error="Username and Password dont match")
 
 
@@ -175,12 +181,14 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('show_login_account_form'))
 
+
 @app.route('/dashboard')
 def show_dashboard_form():
     if 'username' in session:
         current_user = User.query.filter_by(username=session["username"]).first()
-        return render_template('dashboard.html', user_name=current_user.username,  user_role=current_user.user_level)
+        return render_template('dashboard.html', user_name=current_user.username, user_role=current_user.user_level)
     return render_template('user_login.html')
+
 
 @app.route('/admindashboard')
 def show_admin_dashboard_form():
@@ -194,11 +202,14 @@ def show_admin_dashboard_form():
             for user in users:
                 user_info = PersonalInformation.query.filter_by(user_id=user.user_id).first()
                 if user.user_level.name == "SALESPERSON":
-                    user_list.append({"username" : user.username ,"email" : user_info.email, "fullname" : user_info.full_name})
+                    user_list.append(
+                        {"username": user.username, "email": user_info.email, "fullname": user_info.full_name})
             if len(user_list) > 0:
-                return render_template('admin_dashboard2.html', user_name=current_user.username, users=user_list) #Just testing
+                return render_template('admin_dashboard2.html', user_name=current_user.username,
+                                       users=user_list)  # Just testing
             else:
-                return render_template('admin_dashboard2.html', user_name=current_user.username, users=user_list, no_users=True)
+                return render_template('admin_dashboard2.html', user_name=current_user.username, users=user_list,
+                                       no_users=True)
         else:
             return render_template("forbidden.html"), 403
     return render_template('login.html')
@@ -206,12 +217,11 @@ def show_admin_dashboard_form():
 
 @app.route('/deleteuser/<string:username>')
 def delete_user(username):
-
     if 'username' in session:
         current_user = User.query.filter_by(username=session["username"]).first()
         if current_user.user_level.name == "SUPERADMIN":
             del_user = User.query.filter_by(username=username).first()
-            print("USERID -> " +  str(del_user.user_id))
+            print("USERID -> " + str(del_user.user_id))
 
             PersonalInformation.query.filter_by(user_id=del_user.user_id).delete()
             Advertisement.query.filter_by(model_id=del_user.user_id).delete()
@@ -266,6 +276,7 @@ def create_account_salesperson():
     print("Salesperson added")
     return redirect(url_for('show_admin_dashboard_form'))
 
+
 @app.route('/changepassword', methods=['POST'])
 def change_password():
     if 'username' in session:
@@ -282,12 +293,15 @@ def change_password():
                 db.session.commit()
                 flash('Password Changed', 'success')
                 return render_template('changePassword.html')
-                #return redirect(url_for('homepage'))
+                # return redirect(url_for('homepage'))
         else:
             return render_template('changePassword.html', error="Passwords dont match")
 
+
 @app.route('/add_car_advertisement', methods=['POST'])
 def add_car_advertisement():
+    current_user = User.query.filter_by(username=session["username"]).first()
+
     car_name = request.form['car_name']
     car_made = request.form['car_made']
     price = request.form['price']
@@ -301,11 +315,10 @@ def add_car_advertisement():
     fuel_economy = request.form['fuel_economy']
 
     if 'year_of_produce' in request.form:
-        year_of_produce = request.form['year_of_produce'] #Be
+        year_of_produce = request.form['year_of_produce']  # Be
     else:
         current_year = datetime.now().year
         year_of_produce = current_year
-
 
     if 'car_images' in request.files:
         images = request.files.getlist('car_images')
@@ -326,10 +339,10 @@ def add_car_advertisement():
         year=year_of_produce,
         mileage=mileage,
         price=price,
-        description=car_overview, #First Safety Features, then additioanl
+        description=car_overview,  # First Safety Features, then additioanl
         image_url="images/" + images[0].filename,
-        image_url_2="images/" +images[1].filename,
-        image_url_3="images/" +images[2].filename,
+        image_url_2="images/" + images[1].filename,
+        image_url_3="images/" + images[2].filename,
         fuel_type=fuel_economy,
         safety_features=first_string,
         additional_details=second_string
@@ -346,18 +359,51 @@ def add_car_advertisement():
 
     new_ad = Advertisement(
         model_id=new_car.model_id,
-        ad_title="New Car",
-        ad_description="Test",
-        ad_expiry_date=datetime.strptime("2024-4-5", '%Y-%m-%d').date(),  # Convert to datetime object
-        is_new=is_new
+        ad_title=car_made + "  " + car_name,
+        ad_description=first_string + "-" + second_string,
+        ad_expiry_date=datetime.strptime("2024-12-12", '%Y-%m-%d').date(),  # Convert to datetime object
+        is_new=is_new,
+        user_id=current_user.user_id,
+        is_hidden=False
+
     )
     db.session.add(new_ad)
     db.session.commit()
 
     flash('Your car has been added', 'success')
 
-
     return redirect(url_for('account'))
+
+
+@app.route('/show_my_advertisements')
+def show_my_advertisements():
+    if 'username' in session:
+        current_user = User.query.filter_by(username=session["username"]).first()
+        entries = Advertisement.query.filter_by(user_id=current_user.user_id).all()
+        advertisements = []
+        no_advertisements = False
+
+        for entry in entries:
+            if not entry.is_hidden:
+                advertisements.append({"title": entry.ad_title, "id": entry.ad_id, "condition": entry.is_new,
+                                       "expire": entry.ad_expiry_date})
+        if len(advertisements) == 0:
+            no_advertisements = True
+    # for ad in advertisements
+    return render_template('show_my_advertisements.html', advertisements=advertisements, no_advertisements = no_advertisements)
+
+
+@app.route('/delete_ad/<int:advertisement_id>', methods=['POST'])
+def delete_offer(advertisement_id):
+    advertisement = Advertisement.query.filter_by(ad_id=advertisement_id).first()
+
+    advertisement.is_hidden = True
+
+    db.session.commit()
+
+    flash("Advertisement Deleted", "success")
+
+    return redirect(url_for('show_my_advertisements'))
 
 
 @app.route('/homepage', methods=['GET', 'POST'])
@@ -373,7 +419,7 @@ def homepage():
                 CarModel.mileage,
                 CarModel.price,
                 CarModel.image_url
-            ).join(CarModel, Advertisement.model_id == CarModel.model_id).all()
+            ).join(CarModel, Advertisement.model_id == CarModel.model_id).filter(Advertisement.is_hidden==False).all()
             return render_template('homepage.html', ads_with_cars=ads_with_cars)
         else:
             return render_template('user_login.html')
@@ -381,12 +427,11 @@ def homepage():
         return render_template('user_login.html')
 
 
-
 @app.route('/ad/<int:ad_id>', methods=['GET', 'POST'])
 def ad_detail(ad_id):
     if 'username' in session:
         current_user = User.query.filter_by(username=session["username"]).first()
-        if current_user and current_user.user_level == UserLevels.BUYER_SELLER:
+        if current_user and current_user.user_level == UserLevels.BUYER_SELLER or current_user.user_level == UserLevels.SALESPERSON:
             advertisement = Advertisement.query.get_or_404(ad_id)
             car_model = CarModel.query.get_or_404(advertisement.model_id)
 
@@ -395,17 +440,17 @@ def ad_detail(ad_id):
                 email = request.form['email']
                 country = request.form['country']
                 bank_account = float(request.form.get('bank_account', 0))
-                
+
                 # Determine the price to check against the bank account
                 asking_price = float(car_model.price)
                 negotiated_price = None  # Default value
-                
+
                 if not advertisement.is_new:
                     negotiated_price = float(request.form.get('negotiation_price', 0) or 0)
                     price_to_check = negotiated_price if negotiated_price else asking_price
                 else:
                     price_to_check = asking_price
-                
+
                 # Check if the bank account covers the price
                 if bank_account < price_to_check:
                     flash("Insufficient funds in the bank account for this purchase.", "error")
@@ -437,61 +482,50 @@ def ad_detail(ad_id):
     else:
         flash('You must be logged in as a buyer or seller to view this page.')
         return redirect(url_for('user_login'))
-    
 
 
 @app.route('/update_personal_info', methods=['POST'])
 def update_personal_info():
-    
     username = session.get('username')
     if not username:
-        
         return redirect(url_for('login'))
 
-    
     user = User.query.filter_by(username=username).first()
     if not user:
-        
         return redirect(url_for('login'))
 
-    
     personal_info = PersonalInformation.query.filter_by(user_id=user.user_id).first()
     if not personal_info:
-        
         return redirect(url_for('account'))
 
-    
     personal_info.full_name = request.form['full_name']
     personal_info.email = request.form['email']
     personal_info.address = request.form['address']
 
- 
     try:
         db.session.commit()
-        
+
     except Exception as e:
         db.session.rollback()
-        
+
         app.logger.error(f'Error updating personal info: {e}')
 
     return redirect(url_for('account'))
-    
 
 
 @app.route('/agentdashboard')
 def agentdashboard():
     return render_template('agentDisplay.html')
 
+
 @app.route('/account', methods=['GET'])
 def account():
     username = session.get('username')
     if not username:
-        
         return redirect(url_for('login'))
 
     user = User.query.filter_by(username=username).first()
     if not user:
-        
         return redirect(url_for('login'))
 
     personal_info = PersonalInformation.query.filter_by(user_id=user.user_id).first()
@@ -499,37 +533,47 @@ def account():
         # If no PersonalInformation exists, return a 404 error
         abort(404)
 
-    orders = Order.query.filter_by(user_id=user.user_id).join(Advertisement, Advertisement.ad_id == Order.ad_id).join(CarModel, CarModel.model_id == Order.model_id).order_by(Order.order_date.desc()).limit(5).all()
+    orders = Order.query.filter_by(user_id=user.user_id).join(Advertisement, Advertisement.ad_id == Order.ad_id).join(
+        CarModel, CarModel.model_id == Order.model_id).order_by(Order.order_date.desc()).limit(5).all()
     # Render the account page with the current user info
-    return render_template('account.html', user=user, personal_info=personal_info, orders=orders, user_role=user.user_level.name)
+    return render_template('account.html', user=user, personal_info=personal_info, orders=orders,
+                           user_role=user.user_level.name)
+
 
 @app.route('/superadmindashboard')
 def superadmindashboard():
     return render_template('superadmin_dashboard.html')
 
+
 @app.route('/sellerview')
 def sellerview():
     return render_template('sellerview.html')
+
 
 @app.route('/changepassword')
 def changepassword():
     return render_template('changePassword.html')
 
+
 @app.route('/confirmpassword')
 def confirmpassword():
     return render_template('passwordConfirm.html')
+
 
 @app.route('/buyerask')
 def buyerask():
     return render_template('buyernegotiation.html')
 
+
 @app.route('/askconfirm')
 def askconfirm():
     return render_template('negotiationconfirm.html')
 
+
 @app.route('/aboutus')
 def aboutus():
     return render_template('aboutus.html')
+
 
 #with app.app_context():  #Just on Inital RUN
 
@@ -540,6 +584,3 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
-
-
-
